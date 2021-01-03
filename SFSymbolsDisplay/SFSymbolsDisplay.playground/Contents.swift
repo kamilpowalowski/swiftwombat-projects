c4a8f929d7784f57538b8c5b846e7e6d3dfcd9fe
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-01-03 13:44:31 +0100

    SFSymbolsDisplay playground added

---
+++ SFSymbolsDisplay/SFSymbolsDisplay.playground/Contents.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: "sunrise.fill")
+            Text("Swift \(Image(systemName: "sun.max.fill")) Wombat")
+            Image(systemName: "thermometer.sun.fill")
+                .renderingMode(.original)
+                .background(Color.gray)
+            Image(systemName: "moon.circle.fill")
+                .renderingMode(.original)
+                .font(.headline)
+            Image(systemName: "moon.circle")
+                .font(Font.largeTitle.bold())
+            Image(systemName: "moon.circle")
+                .foregroundColor(.green)
+        }
+        .font(.largeTitle)
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+)
