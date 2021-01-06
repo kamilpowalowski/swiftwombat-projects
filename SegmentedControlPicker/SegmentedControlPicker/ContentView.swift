817eb7a131c33c8decfeda44060c7dadcbd337e4
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-01-06 17:03:35 +0100

    SegmentedControlPicker Xcode project added

---
+++ SegmentedControlPicker/SegmentedControlPicker/ContentView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct ContentView: View {
+    private enum Section: String, Identifiable, CaseIterable {
+        case all, favorites, recommended
+        
+        var displayName: String { rawValue.capitalized }
+        
+        var id: String { self.rawValue }
+    }
+    
+    @State private var selectedSection = Section.all
+    
+    var body: some View {
+        VStack {
+            Picker("Selected section:", selection: $selectedSection) {
+                ForEach(Section.allCases) { section in
+                    Text(section.displayName)
+                        .tag(section)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            Spacer()
+            Text("\(selectedSection.displayName)")
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SegmentedControlPicker/SegmentedControlPicker/SegmentedControlPickerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SegmentedControlPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
