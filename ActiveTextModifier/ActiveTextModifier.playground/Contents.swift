bca681efea220651d5c737ca66fef6a6de07ec02
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-01-28 21:02:26 +0100

    ActiveTextModifier Project added

---
+++ ActiveTextModifier/ActiveTextModifier.playground/Contents.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    @State private var isItalic = true
+    
+    var body: some View {
+        Text("Hello ")
+            + Text("Swift Wombat")
+            .italic(isItalic)
+            .active(true, Text.bold)
+            .active(true, { $0.font(.title) })
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+        .frame(width: 300)
+)
---
+++ ActiveTextModifier/ActiveTextModifier.playground/Sources/Text+ActiveModifier.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+public extension Text {
+    func italic(_ active: Bool) -> Text {
+        guard active else { return self }
+        return italic()
+    }
+    
+    func active(
+        _ active: Bool,
+        _ modifier: (Text) -> Text
+    ) -> Text {
+        guard active else { return self }
+        return modifier(self)
+    }
+    
+    func active(
+        _ active: Bool,
+        _ modifier: (Text) -> () -> Text
+    ) -> Text {
+        guard active else { return self }
+        return modifier(self)()
+    }
+}
