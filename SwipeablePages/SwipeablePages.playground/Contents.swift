90546d27f0a07612eb58216dff3acee026f35414
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-02-05 21:58:50 +0100

    SwipeablePages project added

---
+++ SwipeablePages/SwipeablePages.playground/Contents.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Page(
+                title: "Wombat",
+                image: #imageLiteral(resourceName: "wombat.png"),
+                color: Color(hex: 0xF8CF3C)
+            )
+            Page(
+                title: "Koala",
+                image: #imageLiteral(resourceName: "koala.png"),
+                color: Color(hex: 0xF6B6AC)
+            )
+            Page(
+                title: "Platypus",
+                image: #imageLiteral(resourceName: "platypus.png"),
+                color: Color(hex: 0xF26E64)
+            )
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .frame(width: 300, height: 300)
+)
---
+++ SwipeablePages/SwipeablePages.playground/Sources/Page.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+public struct Page: View {
+    let title: String
+    let image: UIImage
+    let color: Color
+    
+    public init(
+        title: String,
+        image: UIImage,
+        color: Color
+    ) {
+        self.title = title
+        self.image = image
+        self.color = color
+    }
+    
+    public var body: some View {
+        VStack {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .padding()
+            Text(title)
+                .bold()
+                .font(.title)
+                .foregroundColor(.white)
+                .padding(.bottom, 32)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(color)
+        .cornerRadius(16)
+        .padding()
+    }
+}
