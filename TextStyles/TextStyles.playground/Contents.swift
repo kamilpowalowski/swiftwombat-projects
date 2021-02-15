ad6b76077405736b71990e65ed7441626a06a312
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-02-15 18:25:51 +0100

    TextStyles project

---
+++ TextStyles/TextStyles.playground/Contents.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Group {
+                Text("italic")
+                    .italic()
+                Text("bold")
+                    .bold()
+                Text("weight .heavy")
+                    .fontWeight(.heavy)
+                Text("font .title")
+                    .font(.title)
+            }
+            Group {
+                Text("strikethrough")
+                    .strikethrough()
+                Text("strikethrough color")
+                    .strikethrough(true, color: .red)
+                Text("underline")
+                    .underline()
+                Text("underline color")
+                    .underline(true, color: .red)
+                Text("foregroundColor")
+                    .foregroundColor(.red)
+                Text("background")
+                    .background(Color.red)
+            }
+            Group {
+                Text("baseline ")
+                    + Text("Off")
+                    .baselineOffset(10)
+                    + Text("set")
+                    .baselineOffset(-10)
+                Text("kerning kerning kerning")
+                    .kerning(1.2)
+                Text("tracking tracking tracking")
+                    .tracking(1.2)
+            }            
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+)
