bb695045d8b2139e6d1705fb87160740ff773ca2
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-02-20 19:22:55 +0100

    Added RoundedCorners example project

---
+++ RoundedCorners/RoundedCorners.playground/Contents.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            text
+                .background(Color.green)
+                .cornerRadius(16)
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(.green)
+                text
+            }
+            text
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .strokeBorder()
+                        .foregroundColor(.green)
+                )
+        }
+    }
+    
+    private var text: some View {
+        Text("Swift Wombat")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+)
