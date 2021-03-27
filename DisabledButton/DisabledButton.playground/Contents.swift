a1a6a2d1f802d50eed340d6251fef980b1d699fb
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-03-27 20:17:27 +0100

    Added disabled button example

---
+++ DisabledButton/DisabledButton.playground/Contents.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    @State var disabled = false
+    var body: some View {
+        VStack {
+            Button("Press me 1", action: { disabled.toggle() })
+            Button("Press me 2", action: {})
+                .disabled(true)
+            VStack {
+                Button("Press me 3", action: {})
+                Button("Press me 4", action: {})
+                    .disabled(false)
+                Button("Press me 5", action: {})
+                    .disabled(true)
+            }
+            .disabled(disabled)
+        }
+
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+)
