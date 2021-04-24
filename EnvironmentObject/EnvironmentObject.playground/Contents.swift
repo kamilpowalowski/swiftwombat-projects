bd125b9aa8ff744fefd83b3d7a5c91a9ae17da7a
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-04-24 17:46:06 +0200

    EnvironmentObject experiments

---
+++ EnvironmentObject/EnvironmentObject.playground/Contents.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import PlaygroundSupport
+
+class Counter: ObservableObject {
+    @Published var value = 0
+}
+
+struct ContentView: View {
+    @StateObject private var counter = Counter()
+    
+    var body: some View {
+        CounterView()
+            .environmentObject(counter)
+    }
+}
+
+struct CounterView: View {
+    @EnvironmentObject private var counter: Counter
+
+    var body: some View {
+        VStack {
+            Text("\(counter.value)")
+            Divider()
+            IncrementView()
+            Divider()
+            DecrementView()
+        }
+    }
+}
+
+struct IncrementView: View {
+    @EnvironmentObject private var counter: Counter
+
+    var body: some View {
+        Button("Increment", action: { counter.value += 1 })
+    }
+}
+
+struct DecrementView: View {
+    @EnvironmentObject private var counter: Counter
+
+    var body: some View {
+        Button("Decrement", action: { counter.value -= 1 })
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+)
