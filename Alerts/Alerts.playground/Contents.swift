e15fc9ecee8203f5fb85e198237590df188e357c
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-05-02 16:54:53 +0200

    Playground with alerts

---
+++ Alerts/Alerts.playground/Contents.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+import PlaygroundSupport
+
+enum Animal: String, CaseIterable, Identifiable {
+    case wombat, koala, platypus
+
+    var id: String { rawValue }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            AnimalPicker()
+            Divider()
+            SimpleAlert()
+        }
+    }
+}
+
+struct AnimalPicker: View {
+    @State private var selectedAnimal: Animal?
+
+    var body: some View {
+        VStack {
+            Text("Please select animal:")
+            button(for: .wombat)
+            button(for: .koala)
+            button(for: .platypus)
+        }
+        .alert(item: $selectedAnimal) { animal in
+            Alert(
+                title: Text("Selected animal:"),
+                message: Text(animal.rawValue.capitalized)
+            )
+        }
+    }
+
+    private func button(for animal: Animal) -> some View {
+        Button(animal.rawValue.capitalized) {
+            selectedAnimal = animal
+        }
+    }
+}
+
+struct SimpleAlert: View {
+    @State private var showAlert = false
+
+    var body: some View {
+        Button("Show alert") {
+            showAlert = true
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Uninstall this application"),
+                message: Text("Do you want to uninstall this application? This action cannot be undone."),
+                primaryButton: .cancel(),
+                secondaryButton: .destructive(Text("Uninstall"))
+            )
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(
+    ContentView()
+        .padding()
+        .frame(width: 400, height: 200)
+)
