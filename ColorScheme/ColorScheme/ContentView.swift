16fccc82db2a191c531d223c37f15f20bc0899a7
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-07-11 19:05:55 +0200

    ColorScheme example

---
+++ ColorScheme/ColorScheme/ContentView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+// Photo by eric anada from Pexels
+// https://www.pexels.com/photo/photo-of-light-bulb-1495580/
+
+struct ContentView: View {
+    @Environment(\.colorScheme) private var colorScheme: ColorScheme
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+            if colorScheme == .dark {
+                Rectangle()
+                    .fill(Color.black.opacity(0.3))
+            }
+            Text("Swift Wombat")
+                .font(.largeTitle)
+                .bold()
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
